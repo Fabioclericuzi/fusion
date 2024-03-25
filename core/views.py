e7a222c579098d57565b0f92b423825c9d3509d6
unknown <fabioclericuzilima@hotmail.com>
2024-03-25 13:18:11 -0300

    Finalização do projeto com a formatação da rotina de envio de email

--- core/views.py
+++ core/views.py
@@ -1,9 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
 from .models import Servico, Funcionario, Recursos
+from django.urls import reverse_lazy
+from .forms import ContatoForm
+from django.contrib import messages
 
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
@@ -14,6 +19,15 @@ class IndexView(TemplateView):
         context['recursos1'] = Recursos.objects.all()[:var]
         context['recursos2'] = Recursos.objects.all()[var:]
         return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, 'E-mail enviado com sucesso')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro ao enviar e-mail')
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
 
 
 
